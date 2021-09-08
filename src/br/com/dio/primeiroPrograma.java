1f91b8eca20902139b3c11d5a6bc23bbe3cc6084
joaquim diniz <JoaquimFco@gmail.com>
2021-09-08 17:35:37 -0300

    primeiro commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+
+        //System.out.println("The World!");
+        Gato gata= new Gato();
+        System.out.println(gata);
+
+    }
+}
